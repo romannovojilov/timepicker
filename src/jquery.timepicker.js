babc3502f5c376f4e1344cad91b5d567922a79b2
Roman Novojilov <romannovojilov@mail.ru>
2018-09-03 20:26:46 +0300

    Added TimePicker classes

--- src/jquery.timepicker.js
+++ src/jquery.timepicker.js
@@ -10,7 +10,7 @@
             this.add = function(fn) {
                 list.push(fn);
             }
-        
+
             this.call = function(sender, eventArgs) {
                 for(var i = 0; i < list.length; i++) {
                     list[i](sender, eventArgs);
@@ -22,56 +22,46 @@
             }
         }
 
-        var onchange = new Event();
+        function TimePickerItem(params) {
+            var self = this;
+            var minval = params.minval || 0;
+            var maxval = params.maxval || 59;
+            var current = params.val || 0;
+            var name = params.name || ('time_item_from_' + minval + '_to_' + maxval);
 
-        var params = [
-            {
-                name: 'hh',
-                value: {
-                    min: 0,
-                    max: 23,
-                    current: 0
-                }
-            },
-            {
-                name: 'mm',
-                value: {
-                    min: 0,
-                    max: 59,
-                    current: 0
-                }
-            },
-            {
-                name: 'ss',
-                value: {
-                    min: 0,
-                    max: 59,
-                    current: 0
-                }
+            var onchange = new Event();
+            this.addObserver = function(callback) {
+                onchange.add(callback);
             }
-        ];
 
-        var wrapper = undefined, 
-            items = undefined;
+            this.setVal = function(val) {
+                current = (val > maxval ? 0 : val < minval ? maxval : val);
+                onchange.call(self, { 
+                    minval : minval, 
+                    maxval : maxval, 
+                    val : current, 
+                    name : name 
+                });
+            }
 
-        var initialize = function() {
-            wrapper = $('<div class="timepicker-box" onclick="event.cancelBubble = true;"></div>');
-            items = [];
-            for(var i = 0; i < params.length; i++) {
+            this.getVal = function() {
+                return current;
+            }
 
+            this.render = function() {
                 var item = $('<div class="timepicker-item"></div>');
-                var checker = $('<div data-minval="' + params[i].value.min + '" data-maxval="' + params[i].value.max + '" class="timepicker-checker">' + params[i].value.current + '</div>');
+                var checker = $('<div data-minval="' + minval + '" data-maxval="' + maxval + '" class="timepicker-checker">' + current + '</div>');
                 checker.bind('click', function() {
-                    $(this).text($(this).attr('data-minval'));
+                    self.setVal(minval);
                 });
                 var toUp = $('<a href="#" class="timepicker-checker-button"></a>');
                 toUp.bind('click', function() {
-                    update($(this).siblings('.timepicker-checker'), true);
+                    self.setVal(current + 1);
                     return false;
                 });
                 var toDown = $('<a href="#" class="timepicker-checker-button"></a>');     
                 toDown.bind('click', function() {
-                    update($(this).siblings('.timepicker-checker'), false);
+                    self.setVal(current - 1);
                     return false;
                 });
 
@@ -79,72 +69,106 @@
                     .append(checker)
                     .append(toDown);
                 
-                items[params[i].name] = item;
-                items[params[i].name].bind('mousewheel DOMMouseScroll', function (event) {
-                    update($(this).find('.timepicker-checker'), event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0);
+                item.bind('mousewheel DOMMouseScroll', function (event) {
+                    self.setVal(event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0 ? current + 1: current -1);
                 });
+
+                self.addObserver(function(sender, args) {
+                    checker.text(args.val);
+                });
+
+                return item;
             }
         }
-        initialize();
 
-        var update = function (sender, isUp) {
-            var val = parseInt($(sender).text());
-            var minVal = parseInt($(sender).attr('data-minval'));
-            var maxVal = parseInt($(sender).attr('data-maxval'));
-            $(sender).text(isUp ? (val + 1 > maxVal ? 0: val + 1): (val - 1 < minVal ? maxVal: val - 1));
-            
+        function TimePicker(params) {
+            var self = this;
 
-            currentInput.val($(sender).text()).focus();
-
-            onchange.call(currentInput, {
-
-            });
-        };
-
-        
-
-        var currentInput = undefined;
-        var render = function(input) {
-            currentInput = input;
-            wrapper.children('.timepicker-spliter').remove();
-            for (var i = 0; i < params.length; i++) {
-                var delta = 1;
-                wrapper.append(items[params[i].name]);
-                if (i + delta < params.length)
-                    wrapper.append('<span class="timepicker-spliter"></span>');
+            var wrapper = $('<div class="timepicker-box" onclick="event.cancelBubble = true;"></div>');
+            this.render = function() {
+                wrapper.html('').hide();
+                for(var i = 0; i < params.length; i++) {
+                    wrapper.append(params[i].render());
+                    if(i+1 < params.length)
+                        wrapper.append('<span class="timepicker-spliter"></span>');
+                }
+                return wrapper;
             }
+
+            this.setPosition = function(x,y) {
+                $(wrapper).css('left', x + 'px')
+                    .css('top', y + 'px');
+            }
+
+            this.show = function() {
+                wrapper.show();
+            }
+
+            this.hide = function() {
+                wrapper.hide();
+            }
+
+        }
+
+        var params = [
+            new TimePickerItem({
+                name: 'hh',
+                maxval: 23
+            }),
+            new TimePickerItem({
+                name: 'mm'
+            }),
+            new TimePickerItem({
+                name: 'ss'
+            }),
+        ];
+
+        var picker = new TimePicker(params); 
+
+        var render = function() {
+            picker.render().appendTo('body');
+        }
+        render();
+        
+        var currentInput = undefined;
+        var initialize = function() {
+            currentInput = $(this);
             setPosition();
-
-            $('body').append(wrapper);
-            wrapper.show();
-
-            return wrapper;
+            picker.show();
         }
 
         var setPosition = function() {
             if(currentInput) 
-                $(wrapper).css('left', ($(currentInput).offset().left) + 'px')
-                    .css('top', ($(currentInput).offset().top + $(currentInput).outerHeight()) + 'px');
+                picker.setPosition($(currentInput).offset().left, $(currentInput).offset().top + $(currentInput).outerHeight());
         }
 
         $(window).bind('resize', setPosition);
         $(document).bind('click', function() {
-            wrapper.hide();
+            picker.hide();
         });
+
+
+        var line = 'hh:mm:ss';
+        for(var i = 0; i < params.length; i++) {
+            params[i].addObserver(function(sender, args) {
+                console.log(args);
+                if(currentInput) {
+                    line = line.replace(args.name, (args.val < 10 ? "0" + args.val : "" + args.val));
+                    currentInput.val(line);
+                }
+            });
+        }
 
         return this.each(function () {
             $(this).attr('onclick', 'event.cancelBubble = true;');
 
-            $(this).bind('focus', function () {
-                render($(this));
-            });
+            $(this).bind('focus', initialize);
 
             $(this).bind('keydown', function (e) {
                 if(e.keyCode == 9) {
-                    wrapper.hide();
+                    picker.hide();
                 }
             });
-
         });
     }
 })(jQuery);
